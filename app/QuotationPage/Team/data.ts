957645761a78865c510331a641ea4a24a4fa2428
Akshay Kumar <akshay.official.dev@gmail.com>
2024-09-13 23:04:11 +0530

    Added more components

---
+++ app/QuotationPage/Team/data.ts
@@ -0,0 +1,60 @@
+import ranbir from '../assets/clients/ranbir.png'
+
+export const team = [
+    {
+        id: 1,
+        img: ranbir,
+        name: 'Ishat Jain',
+        positon: 'Founder',
+        country: 'INDIA',
+        countryLogo: '',
+    },
+    {
+        id: 2,
+        img: ranbir,
+        name: 'Ishat Jain',
+        positon: 'Founder',
+        country: 'INDIA',
+        countryLogo: '',
+    },
+    {
+        id: 3,
+        img: ranbir,
+        name: 'Ishat Jain',
+        positon: 'Founder',
+        country: 'INDIA',
+        countryLogo: '',
+    },
+    {
+        id: 4,
+        img: ranbir,
+        name: 'Ishat Jain',
+        positon: 'Founder',
+        country: 'INDIA',
+        countryLogo: '',
+    },
+    {
+        id: 5,
+        img: ranbir,
+        name: 'Ishat Jain',
+        positon: 'Founder',
+        country: 'INDIA',
+        countryLogo: '',
+    },
+    {
+        id: 6,
+        img: ranbir,
+        name: 'Ishat Jain',
+        positon: 'Founder',
+        country: 'INDIA',
+        countryLogo: '',
+    },
+    {
+        id: 7,
+        img: ranbir,
+        name: 'Ishat Jain',
+        positon: 'Founder',
+        country: 'INDIA',
+        countryLogo: '',
+    },
+]
---
+++ app/QuotationPage/Team/index.tsx
@@ -0,0 +1,25 @@
+import { team } from './data'
+
+const Team = () => {
+    return (
+        <div className="py-20">
+            <h1 className="uppercase font-bold text-5xl text-center pb-10 text-[#78746c]">
+                our team
+            </h1>
+            <div className="grid grid-cols-4 container mx-auto max-w-7xl">
+                {team.map((person) => {
+                    return (
+                        <div className="text-black flex flex-col items-center pb-5 gap-0">
+                            <img src={person.img} alt="" />
+                            <h1>{person.name}</h1>
+                            <p>{person.positon}</p>
+                            <p>{person.country}</p>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default Team
