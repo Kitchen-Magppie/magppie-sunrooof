f515f32e63262c93f056c1ef0244c6e76bbd8d5b
Akshay Kumar <akshay.official.dev@gmail.com>
2024-09-14 15:34:03 +0530

    added small changes

--- app/QuotationPage/Features/index.tsx
+++ app/QuotationPage/Features/index.tsx
@@ -2,12 +2,12 @@ import { features } from './data'
 
 const Features = () => {
     return (
-        <div className="flex flex-col md:flex-row items-start w-full container mx-auto py-20">
+        <div className="flex flex-col lg:flex-row items-start w-full container mx-auto py-20">
             {features.map((feature) => {
                 return (
                     <div
                         key={feature.id}
-                        className="flex flex-col mb-10 md:items-center md:text-center"
+                        className="flex flex-col mb-10 lg:items-center lg:text-center"
                     >
                         <img src={feature.img} className="h-40 w-40" alt="" />
                         <h1 className="text-3xl pt-3">{feature.heading}</h1>
--- app/QuotationPage/Team/index.tsx
+++ app/QuotationPage/Team/index.tsx
@@ -6,7 +6,7 @@ const Team = () => {
             <h1 className="uppercase font-bold text-5xl text-center pb-10 text-[#78746c]">
                 our team
             </h1>
-            <div className="grid grid-cols-2 md:grid-cols-4 container mx-auto max-w-2xl md:max-w-7xl">
+            <div className="grid grid-cols-2 lg:grid-cols-4 container mx-auto max-w-2xl lg:max-w-7xl">
                 {team.map((person) => {
                     return (
                         <div className="text-black flex flex-col items-center pb-10 gap-0">
