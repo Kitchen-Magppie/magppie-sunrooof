895a0fe934e849a354595d47356771842dbc9bd0
Akshay Kumar <akshay.official.dev@gmail.com>
2024-09-14 15:25:07 +0530

    added small changes for UI

---
+++ app/QuotationPage/BeforeAfter/data.ts
@@ -0,0 +1,11 @@
+import beforeOne from '../assets/before-after/before1.png'
+import afterOne from '../assets/before-after/after1.png'
+import beforeTwo from '../assets/before-after/before2.png'
+import afterTwo from '../assets/before-after/after2.png'
+
+export const images = [
+    { id: 1, image: beforeOne },
+    { id: 2, image: afterOne },
+    { id: 3, image: beforeTwo },
+    { id: 4, image: afterTwo },
+]
