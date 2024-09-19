f960632572c6ff5bff8a10ff7138243181b27898
Shivam Singh <roxshivamsingh@gmail.com>
2024-09-19 22:22:23 +0530

    FIX: Minor;

---
+++ app/cms/mocks/quotation.ts
@@ -0,0 +1,12 @@
+export const QuotationMock = {
+    Comparison: {
+        Row1: {
+            Before: '',
+            After: ''
+        },
+        Row2: {
+            Before: '',
+            After: ''
+        },
+    },
+} as const
