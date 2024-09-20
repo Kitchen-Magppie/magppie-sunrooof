c6cbacd68161dd38f6345c6ae78ae46cecd91e9c
Shivam Singh <roxshivamsingh@gmail.com>
2024-09-20 08:40:24 +0530

    ADD: Customer - Mocks;

--- app/cms/mocks/quotation.ts
+++ app/cms/mocks/quotation.ts
@@ -1,4 +1,4 @@
-import quotation from '../../QuotationPage/assets/quotation.png'
+import illustration from '../../QuotationPage/assets/quotation.png'
 import beforeOne from '../../QuotationPage/assets/before-after/before1.png'
 import afterOne from '../../QuotationPage/assets/before-after/after1.png'
 import beforeTwo from '../../QuotationPage/assets/before-after/before2.png'
@@ -9,5 +9,35 @@ export const QuotationMock = {
         Row2: { Before: beforeTwo, After: afterTwo },
     },
     Client: { name: 'Mr. Karthik', remerk: 'Exclusively designed for' },
-    Quotation: { header: 'Quotation', illustration: quotation }
+    Quotation: { header: 'Quotation', illustration },
+    Design2D: {
+        0: {
+            designBy: 'Nishtha',
+            approvedBy: 'Mrinal',
+            deisgn: 'Classical',
+            finish: 'White',
+            callingHeightOnSite: 'Pearl White',
+            afterInstallation: '10ft',
+            yourPlan: '8ft 11 inch',
+            header: 'Board Room',
+            leftImage: '',
+            rightImage: '',
+        },
+        1: {
+            designBy: 'Nishtha',
+            approvedBy: 'Mrinal',
+            deisgn: 'Classical',
+            finish: 'White',
+            callingHeightOnSite: 'French Window',
+            afterInstallation: '',
+            yourPlan: '',
+            header: 'Board Room',
+            leftImage: '',
+            rightImage: '',
+        }
+    },
+    Design3D: {
+        Image1: '',
+        Image2: ''
+    }
 } as const
