01994cfd7b333862d884ba7c099d83b0bfae4263
Shivam Singh <roxshivamsingh@gmail.com>
2024-09-22 18:44:50 +0530

    FIX: Customer > Comparisons - data sync;

--- app/QuotationPage/BeforeAfter/BeforeAfter.tsx
+++ app/QuotationPage/BeforeAfter/BeforeAfter.tsx
@@ -1,27 +1,28 @@
+import { useMemo } from 'react'
 import { LazyLoadImage } from 'react-lazy-load-image-component'
-// import { images } from './data'
-import { QuotationMock as _data } from "../../cms/mocks"
+import { _, TCustomerComponentComparisonDataItem } from '../../../types'
 
-const BeforeAfter = () => {
-    return (
-        <div className="container max-w-5xl py-20 w-full mx-auto">
-            <div className="grid grid-cols-2 place-items-center gap-2 justify-items-center">
-                {_images.map((image, i) => {
-                    return (
-                        <div key={i}>
-                            <LazyLoadImage
-                                effect="blur"
-                                className="lg:w-[500px] lg:h-[500px] mb-2 h-[350px] w-[350px]"
-                                src={image}
-                                alt=""
-                            />
-                        </div>
-                    )
-                })}
-            </div>
+type TProps = { item: TCustomerComponentComparisonDataItem[] }
+const BeforeAfter = (props: TProps) => {
+
+    const images = useMemo(() => (_.values(props.item?.flatMap((item) => _.values(item.image)))), [props.item])
+    return (<div className="container max-w-5xl py-20 w-full mx-auto">
+        <div className="grid grid-cols-2 place-items-center gap-2 justify-items-center">
+            {images.map((image, i) => {
+                return (
+                    <div key={i}>
+                        <LazyLoadImage
+                            effect="blur"
+                            className="lg:w-[500px] lg:h-[500px] mb-2 h-[350px] w-[350px]"
+                            src={image}
+                            alt=""
+                        />
+                    </div>
+                )
+            })}
         </div>
+    </div>
     )
 }
 
-const _images = Object.values(_data.Comparison)?.flatMap((item) => (Object.values(item)))
 export default BeforeAfter
--- app/QuotationPage/index.tsx
+++ app/QuotationPage/index.tsx
@@ -2,26 +2,44 @@
 // import ThreedDesigns from './3dDesigns'
 import About from './About'
 import BeforeAfter from './BeforeAfter/BeforeAfter'
-import BuyingJourney from './BuyingJourney'
 import ClientName from './ClientName'
 import Clients from './Clients'
 import DesignedBy from './DesignedBy'
 import Features from './Features'
-import Footer from './Footer'
+// import Footer from './Footer'
 import Guarantee from './Guarantee'
 import Header from './Header'
 import ProjectDetails from './ProjectDetails'
 import Quotation from './Quotation'
 import Team from './Team'
 import TermsandConditions from './TermsandConditions/TermsandConditions'
+import FooterFinal from './FooterFinal'
+import BuyingJourney from './BuyingJourney'
+import Navbar from './Navbar'
+import { useFirebaseCmsCustomerListener } from '../cms/utils/firebase'
+import { useAppSelector } from '../../redux'
+import { PageProgress } from '../../components'
+import { useMemo } from 'react'
+import { DEFAULT_CUSTOMER } from '../cms/mocks'
+import { CustomerComponentEnum } from '../../types'
 
 const QuotationPage = () => {
+    useFirebaseCmsCustomerListener()
+    const { loading, value } = useAppSelector((state) => state.Cms.Customer);
+
+    const currentCustomer = useMemo(() => value.find((row) => row.customerId === DEFAULT_CUSTOMER.customerId), [value])
+    // console.log(loading, value)
+    console.log(currentCustomer)
+    if (loading) {
+        return <PageProgress />
+    }
     return (
-        <>
+        <div className='overflow-x-hidden'>
+            <Navbar />
             <Header />
             <About />
             <Clients />
-            <BeforeAfter />
+            <BeforeAfter item={currentCustomer?.components?.find(({ value }) => value === CustomerComponentEnum.Comparison).data} />
             <Features />
             <DesignedBy />
             <Team />
@@ -33,8 +51,9 @@ const QuotationPage = () => {
             <TermsandConditions />
             <Guarantee />
             <BuyingJourney />
-            <Footer />
-        </>
+            {/* <Footer /> */}
+            <FooterFinal />
+        </div>
     )
 }
 
