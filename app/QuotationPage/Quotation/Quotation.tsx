fea3dfb7845ca664e200bf46b5bcc91a747eaed2
Akshay Kumar <akshay.official.dev@gmail.com>
2024-09-25 20:56:58 +0530

    small changes

--- app/QuotationPage/Quotation/Quotation.tsx
+++ app/QuotationPage/Quotation/Quotation.tsx
@@ -4,16 +4,21 @@ import ClientDetails from './ClientDetails'
 
 const Quotation = () => {
     return (
-        <div className="flex flex-col justify-center items-center py-10" id='quotation'>
+        <div
+            className="flex flex-col justify-center items-center py-10"
+            id="quotation"
+        >
             <h1 className="text-5xl mb-10">{_data.Quotation.header}</h1>
             <div className="flex flex-col items-center justify center px-24">
                 <ClientDetails />
-                <LazyLoadImage
-                    effect="blur"
-                    src={_data.Quotation.illustration}
-                    className=" lg:w-full lg:h-full object-cover quotation-image"
-                    alt=""
-                />
+                <div className="flex mt-5 h-full w-full">
+                    <LazyLoadImage
+                        effect="blur"
+                        src={_data.Quotation.illustration}
+                        className=" lg:w-full lg:h-full object-contain quotation-image"
+                        alt=""
+                    />
+                </div>
             </div>
         </div>
     )
