2fd5c9d5cb79c6aded70e7ebcbe62fa113280925
Akshay Kumar <akshay.official.dev@gmail.com>
2024-10-15 13:25:38 +0530

    Added small change

--- app/QuotationPage/Quotation/Quotation.tsx
+++ app/QuotationPage/Quotation/Quotation.tsx
@@ -8,13 +8,13 @@ const Quotation = (props: TProps) => {
             className="flex flex-col justify-center py-20 items-center bg-gray-100"
             id="quotation"
         >
-            <h1 className="text-6xl pb-16 w-full text-center uppercase text-[#78746c]">Quotation</h1>
+            <h1 className="text-6xl pb-10 w-full text-center uppercase text-[#78746c]">Quotation</h1>
             <div className="flex flex-col items-center justify center px-24">
                 <ClientDetails name={props.name} item={props.item} />
-                <div className='h-[400px] w-[700px] lg:w-full lg:h-full'>
+                <div className='w-[700px] lg:w-full h-full'>
                     <LazyLoadImage
                         effect="blur"
-                        src={props.item.data.invoiceUrl}
+                        // src={props.item.data.invoiceUrl}
                         className=" lg:w-full lg:h-full object-contain rounded-lg quotation-image mt-5"
                         alt=""
                     />
