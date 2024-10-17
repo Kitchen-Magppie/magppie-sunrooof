96f4ccdd8388f727278e2137470d0575ac69f915
Shivam Singh <roxshivamsingh@gmail.com>
2024-10-17 12:01:44 +0530

    FIX: Minor issues;

--- app/QuotationGenerator/Containers/Home.tsx
+++ app/QuotationGenerator/Containers/Home.tsx
@@ -1,11 +1,17 @@
+import { useState } from 'react'
 import Buttons from '../components/Buttons'
 import Hero from '../components/Hero'
 
+type TCorpus = { file?: File }
 const Home = () => {
+    const [corpus, setCorpus] = useState<TCorpus>({})
+    console.log(corpus)
     return (
         <div className="flex flex-col justify-between items-center min-h-screen">
             <Hero />
-            <Buttons />
+            <Buttons
+                onChangeFile={(file) => { setCorpus((prev) => ({ ...prev, file })) }}
+            />
         </div>
     )
 }
