ccf894f648a7a4a2b2d9ddf2387a52231f14dc9a
Shivam Singh <roxshivamsingh@gmail.com>
2024-10-20 18:47:40 +0530

    FIX: Presentation - minor;

--- redux/index.ts
+++ redux/index.ts
@@ -7,6 +7,6 @@ import {
 import type { RootState, AppDispatch } from "./store"
 
 export * from "./store"
-
+export * from "./../app/cms/redux/slices"
 export const useAppDispatch = () => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
