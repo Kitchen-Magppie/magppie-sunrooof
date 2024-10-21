b24eaa81d8a18249ebe2fe85340c70ceb07e7367
Shivam Singh <roxshivamsingh@gmail.com>
2024-10-21 11:12:42 +0530

    FIX: Presentation - minor;

--- app/QuotationGenerator/Containers/QuotationCanvas/QuotationCanvas.tsx
+++ app/QuotationGenerator/Containers/QuotationCanvas/QuotationCanvas.tsx
@@ -147,7 +147,6 @@ function QuotationCanvas() {
 
                 <input className="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     value={measurement.quantity}
-
                     onChange={(e) => {
                         setMeasurement((prev) => ({ ...prev, quantity: Number(e.target.value) }))
                     }}
