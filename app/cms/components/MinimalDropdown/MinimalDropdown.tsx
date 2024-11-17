9726aac6906d712bff4cabe94d5b4ec2e79dbd98
Shivam Singh <roxshivamsingh@gmail.com>
2024-11-17 20:12:58 +0530

    ADD: CMS > Form - common dropdown;

--- app/cms/components/MinimalDropdown/MinimalDropdown.tsx
+++ app/cms/components/MinimalDropdown/MinimalDropdown.tsx
@@ -1,31 +1,33 @@
-// import Select, { ActionMeta } from "react-select"
+import Select, { ActionMeta } from "react-select"
 
-// type TProps = { onChange: (newValue: unknown, actionMeta: ActionMeta<any>) => void }
-// function MinimalDropdown(props: TProps) {
-//     return (<Select
-//         theme={(theme) => ({
-//             ...theme,
-//             borderRadius: 6,
-//             colors: {
-//                 ...theme.colors,
-//                 text: "white",
-//                 primary25: "#3F51B5",
-//                 primary: "#3F51B5",
-//             },
-//         })}
-//         classNames={{
-//             control: () => AUTOCOMPLETE_STYLE,
-//         }}
-//         onChange={(e) => {
-//             setValue(`components.${i}.data`, e.value);
-//         }}
-//         defaultValue={CUSTOMER_COMPONENT_FEATURE_OPTIONS?.find(
-//             ({ value }) => value === data.data
-//         )}
-//         options={CUSTOMER_COMPONENT_FEATURE_OPTIONS}
-//     />);
-// }
+function MinimalDropdown(props: TProps) {
+    return (<Select
+        theme={(theme) => ({
+            ...theme,
+            borderRadius: 6,
+            colors: {
+                ...theme.colors,
+                text: "white",
+                primary25: "#3F51B5",
+                primary: "#3F51B5",
+            },
+        })}
+        placeholder={props?.placeholder}
+        classNames={{ control: () => AUTOCOMPLETE_STYLE }}
+        onChange={props.onChange}
+        defaultValue={props.defaultValue}
+        options={props.options}
+    />);
+}
 
-// export default MinimalDropdown;
-// const AUTOCOMPLETE_STYLE =
-//     "mt-1 block w-full py-1 border border-gray-300 rounded-xl shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm";
+export default MinimalDropdown;
+type TValueItem = { label: string, value: string }
+type TProps = {
+    onChange: (newValue: unknown, actionMeta: ActionMeta<unknown>) => void,
+    defaultValue?: { label: string, value: string },
+    options: TValueItem[],
+    placeholder?: string
+
+}
+const AUTOCOMPLETE_STYLE =
+    "mt-1 block w-full py-1 border border-gray-300 rounded-xl shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm";
