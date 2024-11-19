31dda8a9f6572027c526124c9d29cfb7a4472553
Shivam Singh <roxshivamsingh@gmail.com>
2024-11-20 00:59:20 +0530

    ADD: P5 > VanillaJS - pipeline;

--- src/vanilla.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
-ReactDOM.createRoot(document.getElementById('vanilla-root')!).render(
-    <React.StrictMode>
-        Vanilla JS
-    </React.StrictMode>,
-)
