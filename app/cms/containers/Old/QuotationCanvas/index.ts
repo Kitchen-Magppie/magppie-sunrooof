05e7482fff821697e5ebd01c3a34571c1bbda20a
Akshay Kumar <akshay.official.dev@gmail.com>
2024-11-19 21:41:33 +0530

    code pushed

--- app/cms/containers/Old/QuotationCanvas/index.ts
+++ app/cms/containers/Old/QuotationCanvas/index.ts
@@ -1,4 +1,11 @@
-import QuotationCanvas from './QuotationCanvas'
-
-export { default } from './QuotationCanvas'
-export { QuotationCanvas }
+import QuotationCanvas from "./QuotationCanvas"
+import { QuotationCanvasAlert } from "./QuotationCanvasAlert"
+import QuotationCanvasEditAction from "./QuotationCanvasEditAction"
+import QuotationCanvasUnitMeasurementAction from "./QuotationCanvasUnitMeasurementAction"
+export { default } from "./QuotationCanvas"
+export {
+    QuotationCanvas,
+    QuotationCanvasAlert,
+    QuotationCanvasEditAction,
+    QuotationCanvasUnitMeasurementAction
+};
